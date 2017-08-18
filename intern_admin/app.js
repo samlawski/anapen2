6d23d00f7a88fd8cf4f9ddb0618dc2c713afa167
Sam <sam.bogu@gmail.com>
2017-08-18 22:12:45 +0200

    internal area working

---
+++ intern_admin/app.js
@@ -0,0 +1,99 @@
+// ********************* //
+// ****** CLASSES ****** //
+// ********************* //
+
+function randomString(){
+  return Math.random().toString(36).substring(7);
+}
+
+// Object List
+function initializePageContent(){
+  firebase.database().ref().once("value", function(snap){
+    var key = Object.keys(snap.val())[0];
+    content = new PageContent(key);
+  });
+};
+
+function PageContent(key){
+  this.initializeWithKey(key);
+  this.loadExistingContent();
+  this.initializeSaveButton(key);
+};
+PageContent.prototype.initializeWithKey = function(key){
+  // Get the subfolder with the given key
+  this.database_parent = firebase.database().ref(key);
+};
+PageContent.prototype.loadExistingContent = function(){
+  // Get the `body` property and add it to the text editor
+  this.database_parent.once('value', function(snapshot){
+    console.log(snapshot.val());
+    document.getElementById("edit").classList.remove('hidden');
+    tinymce.activeEditor.setContent(snapshot.val().body);
+    prependServerURLtoEditorImages();
+  });
+};
+PageContent.prototype.initializeSaveButton = function(key){
+  document.getElementById('input_submit').onclick = function(){
+    firebase.database().ref(key).update({
+      body: tinymce.activeEditor.getContent()
+    });
+    initializePageContent();
+    document.getElementById('input_submit_confirmation').classList.remove('hidden');
+    setTimeout(function(){
+      document.getElementById('input_submit_confirmation').classList.add('hidden');
+    }, 5000);
+  }
+};
+
+// Prepend URL to images in textarea and titelimage
+function prependServerURL(img_element){
+  var server_url = "/";
+  img_element.src = img_element.src.replace(/.*(?=uploads\/)/, server_url);
+};
+
+function prependServerURLtoEditorImages(){
+  for (image_element of tinymce.activeEditor.selection.getNode().querySelectorAll('img')) {
+    prependServerURL(image_element);
+  }
+};
+
+var content;
+window.onload = function(){
+  // Firebase authantication
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      if(typeof content == "undefined"){
+        // Toggle views
+        document.querySelector('main').classList.remove('hidden');
+        document.getElementById('require_login').classList.add('hidden');
+        // Get content from firebase
+        initializePageContent();
+      }
+    } else {
+      document.getElementById('require_login').classList.remove('hidden');
+      document.querySelector('main').classList.add('hidden');
+    }
+  });
+  // Textarea initialize
+  tinymce.init({
+    selector:'textarea',
+    menubar: false,
+    toolbar: "undo redo | bold italic underline | removeformat | alignleft aligncenter alignright bullist numlist outdent indent | link image media",
+    height: 300,
+    plugins: "image link media",
+    image_prepend_url: "http://gentle-raven.cloudvent.net/"
+  });
+
+  // Listen: Admin access
+  document.getElementById('admin_access_submit').onclick = function(){
+    var password = document.getElementById('admin_access_pw').value;
+    firebase.auth().signInWithEmailAndPassword('admin_access@bioprojet.de', password).then(function(){
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+  };
+}
---
+++ js/intern.js
@@ -0,0 +1,54 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDkOT5kb42Olm6_54TBVRErqseT_hqZNGw",
+  authDomain: "anapen2-88bb5.firebaseapp.com",
+  databaseURL: "https://anapen2-88bb5.firebaseio.com",
+  projectId: "anapen2-88bb5",
+  storageBucket: "anapen2-88bb5.appspot.com",
+  messagingSenderId: "662667251692"
+};
+firebase.initializeApp(config);
+
+// Check if parameter from DocCheck exist
+if(typeof getUrlParameter('key') != "undefined"){
+  sessionStorage.doccheck_key = getUrlParameter("key");
+  window.history.replaceState('produkte-login', 'Produkte', '/intern');
+};
+
+// Append DocCheck login form
+function appendLoginForm(){
+  $('.intern__content').html(
+    '<div class="text-center">' +
+      '<div class="lead">Die folgenden Inhalte sind nur für Ärzte und Apotheker zugänglich. Bitte loggen Sie sich ein und/oder klicken Sie auf "Weiter", um die Inhalte sehen zu können.</div>' +
+      '<iframe align="center" frameborder="0" scrolling="no" width="311" height="188" name="dc_login_iframe" id="dc_login_iframe" src="https://login.doccheck.com/code/de/2000000010799/m_red/" ><a href="https://login.doccheck.com/code/de/2000000010799/m_red/" target="_blank">LOGIN</a></iframe>' +
+    '</div>'
+  );
+};
+
+// Check if doccheck_key already exists in session storage
+if(sessionStorage.doccheck_key && sessionStorage.doccheck_key.length > 0){
+  // Get page content form Firebase using the doccheck_key as a path
+  firebase.database().ref(sessionStorage.doccheck_key).once('value', function(snapshot){
+    if(snapshot.val()){
+      // Append body
+      $('.intern__content').append(snapshot.val().body);
+
+      // dynamically adjust HTML in Beschreibung
+      $(".intern__content a").attr('target', '_new');
+    }else{
+      appendLoginForm();
+    }
+  }).then(function(success){});
+}else{
+  // Display Admin options inside vidual editor
+  if (window.location.host === "app.cloudcannon.com") {
+    $('.intern__content').html(
+      '<div class="text-center" style="border: 3px solid #5bc0de; padding: 20px;">' +
+        '<div class="lead">Die Produkte sind in einer separaten, geschützten Datenbank gesichert und können daher nur über den folgenden Button bearbeitet werden:</div>' +
+        '<a href="/intern_admin" class="btn btn-info" style="color: white"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Produkte Bearbeiten</a>' +
+      '</div>'
+    )
+  }else{
+    appendLoginForm();
+  }
+};
